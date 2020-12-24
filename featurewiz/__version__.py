37b679ebe59cd5441c7f588347ca1a5642f8e74f
Auto_ViML <rsesha2001@yahoo.com>
2020-12-23 22:21:47 -0500

    Updated README with feature engineering notes.

--- featurewiz/__version__.py
+++ featurewiz/__version__.py
@@ -3,8 +3,8 @@
 
 __title__ = "featurewiz"
 __author__ = "Ram Seshadri"
-__description__ = "Fast Feature Selection for any data set, any size"
+__description__ = "Fast Feature Engineering and Feature Selection for any data set, any size"
 __url__ = "https://github.com/Auto_ViML/featurewiz.git"
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __license__ = "Apache License 2.0"
 __copyright__ = "2020 Google"
